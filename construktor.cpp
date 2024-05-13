8475901b8905208fc569e902b8341e3084ea0ec3
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-13 15:33:42 +0700

    membuat class mahasiswa pada construktor

--- construktor.cpp
+++ construktor.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+    void cetak();
+};
